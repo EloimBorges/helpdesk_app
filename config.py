1926a3a86fd9d79071ed5e16223c116de71b9dd8
EloimBorges <Borges.eloim@gmail.com>
2025-12-15 00:56:45 -0400

    Quickfix to supporting documents and file organization

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "3010")
+SECRET_KEY = os.getenv("SECRET_KEY", "secreto")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
